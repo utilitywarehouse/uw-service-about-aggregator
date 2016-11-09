43f941c952d712a038e8dfdf956cd5c290042b9e
Catalin Ilea <cilea@utilitywarehouse.co.uk>
2016-11-09 14:15:22 +0200

    Added confluence exporter

--- exporter.go
+++ exporter.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"sync"
 )
+
+const confluenceTemplatePath = "confluence.html"
 
 type exporterService struct {
 	exporters []exporter
@@ -93,4 +98,127 @@ func (h *httpExporter) htmlHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Couldn't render template file for html response"))
 		return
 	}
+}
+
+func newConfluenceExporter(confluenceHost string, confluenceCredentials string, confluencePageID string, client httpClient) (*confluenceExporter, error) {
+	if confluenceHost == "" {
+		return nil, fmt.Errorf("confluenceHost is required")
+	}
+	if confluencePageID == "" {
+		return nil, fmt.Errorf("confluencePageID is required")
+	}
+	return &confluenceExporter{
+		confluenceHost:        confluenceHost,
+		confluenceCredentials: confluenceCredentials,
+		confluencePageID:      confluencePageID,
+		client:                client,
+		mutex:                 sync.Mutex{},
+		abouts:                make(map[string]about)}, nil
+}
+
+type confluenceExporter struct {
+	confluenceHost        string
+	confluenceCredentials string
+	confluencePageID      string
+	client                httpClient
+	mutex                 sync.Mutex //protects abouts
+	abouts                map[string]about
+}
+
+func (h *confluenceExporter) handle(ab about) error {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+	h.abouts[ab.Service.Name] = ab
+	a := []about{}
+	for _, about := range h.abouts {
+		a = append(a, about)
+	}
+	var b bytes.Buffer
+	mainTemplate, err := template.ParseFiles(confluenceTemplatePath)
+	if err != nil {
+		return fmt.Errorf("Couldn't find template file for confluence page body: (%v)", err)
+	}
+	if err = mainTemplate.Execute(&b, struct{ Abouts []about }{Abouts: a}); err != nil {
+		return fmt.Errorf("Couldn't render template file for confluence page body: (%v)", err)
+	}
+
+	page, err := h.getConfluencePage(h.confluencePageID)
+	if err != nil {
+		return fmt.Errorf("Could not get confluence page with ID %v: (%v)", h.confluencePageID, err)
+	}
+	page.Body = body{storage{Value: b.String(), Representation: "storage"}}
+	page.Version.Number = page.Version.Number + 1
+
+	if err = h.updateConfluencePage(page); err != nil {
+		return fmt.Errorf("Could not update confluence page with ID %v: (%v)", h.confluencePageID, err)
+	}
+	return nil
+}
+
+func (h *confluenceExporter) getConfluencePage(pageID string) (confluencePage, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/wiki/rest/api/content/%s", h.confluenceHost, h.confluencePageID), nil)
+	if err != nil {
+		return confluencePage{}, fmt.Errorf("Could not get response from %v: (%v)", req.URL.String(), err)
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Basic %s", h.confluenceCredentials))
+	resp, err := h.client.Do(req)
+	if err != nil {
+		return confluencePage{}, fmt.Errorf("Could not get response from %v: (%v)", req.URL.String(), err)
+	}
+	defer func() {
+		io.Copy(ioutil.Discard, resp.Body)
+		resp.Body.Close()
+	}()
+	if resp.StatusCode != http.StatusOK {
+		return confluencePage{}, fmt.Errorf("Confluence api returned status %d", resp.StatusCode)
+	}
+	dec := json.NewDecoder(resp.Body)
+	var page confluencePage
+	if err := dec.Decode(&page); err != nil {
+		return confluencePage{}, fmt.Errorf("Error decoding confluence response: (%v)", err)
+	}
+	return page, nil
+}
+
+func (h *confluenceExporter) updateConfluencePage(newPage confluencePage) error {
+	payload := new(bytes.Buffer)
+	json.NewEncoder(payload).Encode(newPage)
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/wiki/rest/api/content/%s", h.confluenceHost, h.confluencePageID), payload)
+	if err != nil {
+		return fmt.Errorf("Could not get response from %v: (%v)", req.URL.String(), err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Basic %s", h.confluenceCredentials))
+	resp, err := h.client.Do(req)
+	if err != nil {
+		return fmt.Errorf("Could not get response from %v: (%v)", req.URL.String(), err)
+	}
+	defer func() {
+		io.Copy(ioutil.Discard, resp.Body)
+		resp.Body.Close()
+	}()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Confluence api returned status %d", resp.StatusCode)
+	}
+	return nil
+}
+
+type confluencePage struct {
+	Type    string  `json:"type"`
+	Title   string  `json:"title"`
+	Version version `json:"version"`
+	Body    body    `json:"body"`
+}
+
+type version struct {
+	Number int `json:"number"`
+}
+
+type body struct {
+	Storage storage `json:"storage"`
+}
+
+type storage struct {
+	Value          string `json:"value"`
+	Representation string `json:"representation"`
 }
--- exporter_test.go
+++ exporter_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -12,7 +14,7 @@ import (
 )
 
 const htmlResponse = "<!DOCTYPE html>\n<head>\n    <title>UW Documentation</title>\n</head>\n<body>\n<h1>UW Documented services</h1>\n<table style='font-size: 10pt; font-family: MONOSPACE;'>\n    \n    \n    <tr>\n        <td><a href=\"/../../../billing/services/uw-service-refdata:80/__/about\">billing.uw-service-refdata</a></td>\n    </tr>\n    \n    \n</table>\n</body>\n</html>"
-const jsonResponse = "[{\"Service\":{\"Name\":\"uw-service-refdata\",\"Namespace\":\"billing\",\"BaseURL\":\"\"},\"Doc\":null}]"
+const jsonResponse = "[{\"Service\":{\"Name\":\"uw-service-refdata\",\"Namespace\":\"billing\",\"BaseURL\":\"\"},\"Doc\":{\"name\":\"uw-service-refdata\",\"description\":\"uw-service-refdata\",\"owners\":[{\"name\":\"Billing\",\"slack\":\"#billing\"}],\"links\":[{\"url\":\"http://readme\",\"description\":\"readme\"}],\"build-info\":{\"revision\":\"revision\"}}}]"
 
 func TestExporterService(t *testing.T) {
 	errors := make(chan error, 10)
@@ -48,8 +50,17 @@ func TestHTTPExporterHandler(t *testing.T) {
 		contentType string // Contents of the Content-Type header
 		body        string
 	}{
-		{"Success html", newRequest("GET", "/__/about", "text/html"), createHTTPExporterAndHandle(about{Service: service{Name: "uw-service-refdata", Namespace: "billing"}}), http.StatusOK, "text/html", htmlResponse},
-		{"Success json", newRequest("GET", "/__/about", "application/json"), createHTTPExporterAndHandle(about{Service: service{Name: "uw-service-refdata", Namespace: "billing"}}), http.StatusOK, "application/json", jsonResponse},
+		{"Success html", newRequest("GET", "/__/about", "text/html", nil), createHTTPExporterAndHandle(about{Service: service{Name: "uw-service-refdata", Namespace: "billing"}}), http.StatusOK, "text/html", htmlResponse},
+		{"Success json", newRequest("GET", "/__/about", "application/json", nil), createHTTPExporterAndHandle(about{
+			Service: service{Name: "uw-service-refdata", Namespace: "billing"},
+			Doc: doc{
+				Name:        "uw-service-refdata",
+				Description: "uw-service-refdata",
+				Owners:      []owner{owner{Name: "Billing", Slack: "#billing"}},
+				Links:       []link{link{URL: "http://readme", Description: "readme"}},
+				BuildInfo:   buildInfo{Revision: "revision"},
+			}}),
+			http.StatusOK, "application/json", jsonResponse},
 	}
 
 	for _, test := range tests {
@@ -66,8 +77,8 @@ func createHTTPExporterAndHandle(about about) *httpExporter {
 	return httpExporter
 }
 
-func newRequest(method, url string, accept string) *http.Request {
-	req, err := http.NewRequest(method, url, nil)
+func newRequest(method, url string, accept string, body io.Reader) *http.Request {
+	req, err := http.NewRequest(method, url, body)
 	req.Header.Add("Accept", accept)
 	if err != nil {
 		panic(err)
@@ -79,4 +90,151 @@ func router(e *httpExporter) *mux.Router {
 	m := mux.NewRouter()
 	m.HandleFunc("/__/about", e.handleHTTP).Methods("GET")
 	return m
+}
+
+const confluenceURL = "https://utilitywarehouse.atlassian.net"
+const confluencePageID = "1234"
+const confluenceGetPageResponse = "{\"type\":\"page\",\"title\":\"some page\",\"version\":{\"number\":%d}}"
+const confluenceCredentials = "credentials"
+
+func TestConfluenceExporterHandler(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		name   string
+		ab     about
+		client mockedClient
+		err    error
+	}{
+		{"Success - incrementing page version on update",
+			about{
+				Service: service{Name: "uw-service-refdata", Namespace: "billing"},
+				Doc: doc{
+					Name:        "uw-service-refdata",
+					Description: "uw-service-refdata",
+					Owners:      []owner{owner{Name: "Billing", Slack: "#billing"}},
+					Links:       []link{link{URL: "http://readme", Description: "readme"}},
+					BuildInfo:   buildInfo{Revision: "revision"},
+				}},
+			mockedClient{assert, map[string]httpCall{
+				"GET": httpCall{req: confluencePageRequest{confluenceURL + "/wiki/rest/api/content/" + confluencePageID, 1}, resp: http.Response{StatusCode: 200, Body: ioutil.NopCloser(strings.NewReader(confluencePageResponse(1)))}, err: nil},
+				"PUT": httpCall{req: confluencePageRequest{confluenceURL + "/wiki/rest/api/content/" + confluencePageID, 2}, resp: http.Response{StatusCode: 200, Body: ioutil.NopCloser(strings.NewReader("ok"))}, err: nil},
+			}}, nil,
+		},
+		{"Failure - confluence get page request error - confluence unreachable",
+			about{
+				Service: service{Name: "uw-service-refdata", Namespace: "billing"},
+				Doc: doc{
+					Name:        "uw-service-refdata",
+					Description: "uw-service-refdata",
+					Owners:      []owner{owner{Name: "Billing", Slack: "#billing"}},
+					Links:       []link{link{URL: "http://readme", Description: "readme"}},
+					BuildInfo:   buildInfo{Revision: "revision"},
+				}},
+			mockedClient{assert, map[string]httpCall{
+				"GET": httpCall{req: confluencePageRequest{confluenceURL + "/wiki/rest/api/content/" + confluencePageID, 1}, resp: http.Response{StatusCode: 200, Body: ioutil.NopCloser(strings.NewReader(confluencePageResponse(1)))}, err: fmt.Errorf("host unreachable")},
+				"PUT": httpCall{req: confluencePageRequest{confluenceURL + "/wiki/rest/api/content/" + confluencePageID, 2}, resp: http.Response{StatusCode: 200, Body: ioutil.NopCloser(strings.NewReader("ok"))}, err: nil},
+			}}, fmt.Errorf("Could not get confluence page with ID 1234: (Could not get response from https://utilitywarehouse.atlassian.net/wiki/rest/api/content/1234: (host unreachable))"),
+		},
+		{"Failure - confluence get page request error - bad json format",
+			about{
+				Service: service{Name: "uw-service-refdata", Namespace: "billing"},
+				Doc: doc{
+					Name:        "uw-service-refdata",
+					Description: "uw-service-refdata",
+					Owners:      []owner{owner{Name: "Billing", Slack: "#billing"}},
+					Links:       []link{link{URL: "http://readme", Description: "readme"}},
+					BuildInfo:   buildInfo{Revision: "revision"},
+				}},
+			mockedClient{assert, map[string]httpCall{
+				"GET": httpCall{req: confluencePageRequest{confluenceURL + "/wiki/rest/api/content/" + confluencePageID, 1}, resp: http.Response{StatusCode: 200, Body: ioutil.NopCloser(strings.NewReader("not json"))}, err: nil},
+				"PUT": httpCall{req: confluencePageRequest{confluenceURL + "/wiki/rest/api/content/" + confluencePageID, 2}, resp: http.Response{StatusCode: 200, Body: ioutil.NopCloser(strings.NewReader("ok"))}, err: nil},
+			}}, fmt.Errorf("Could not get confluence page with ID 1234: (Error decoding confluence response: (invalid character 'o' in literal null (expecting 'u')))"),
+		},
+		{"Failure - confluence get page request error - non 200",
+			about{
+				Service: service{Name: "uw-service-refdata", Namespace: "billing"},
+				Doc: doc{
+					Name:        "uw-service-refdata",
+					Description: "uw-service-refdata",
+					Owners:      []owner{owner{Name: "Billing", Slack: "#billing"}},
+					Links:       []link{link{URL: "http://readme", Description: "readme"}},
+					BuildInfo:   buildInfo{Revision: "revision"},
+				}},
+			mockedClient{assert, map[string]httpCall{
+				"GET": httpCall{req: confluencePageRequest{confluenceURL + "/wiki/rest/api/content/" + confluencePageID, 1}, resp: http.Response{StatusCode: 500, Body: ioutil.NopCloser(strings.NewReader("not json"))}, err: nil},
+				"PUT": httpCall{req: confluencePageRequest{confluenceURL + "/wiki/rest/api/content/" + confluencePageID, 2}, resp: http.Response{StatusCode: 200, Body: ioutil.NopCloser(strings.NewReader("ok"))}, err: nil},
+			}}, fmt.Errorf("Could not get confluence page with ID 1234: (Confluence api returned status 500)"),
+		},
+		{"Failure - confluence page update request error - confluence unreachable",
+			about{
+				Service: service{Name: "uw-service-refdata", Namespace: "billing"},
+				Doc: doc{
+					Name:        "uw-service-refdata",
+					Description: "uw-service-refdata",
+					Owners:      []owner{owner{Name: "Billing", Slack: "#billing"}},
+					Links:       []link{link{URL: "http://readme", Description: "readme"}},
+					BuildInfo:   buildInfo{Revision: "revision"},
+				}},
+			mockedClient{assert, map[string]httpCall{
+				"GET": httpCall{req: confluencePageRequest{confluenceURL + "/wiki/rest/api/content/" + confluencePageID, 1}, resp: http.Response{StatusCode: 200, Body: ioutil.NopCloser(strings.NewReader(confluencePageResponse(1)))}, err: nil},
+				"PUT": httpCall{req: confluencePageRequest{confluenceURL + "/wiki/rest/api/content/" + confluencePageID, 2}, resp: http.Response{StatusCode: 200, Body: ioutil.NopCloser(strings.NewReader("ok"))}, err: fmt.Errorf("host unreachable")},
+			}}, fmt.Errorf("Could not update confluence page with ID 1234: (Could not get response from https://utilitywarehouse.atlassian.net/wiki/rest/api/content/1234: (host unreachable))"),
+		},
+		{"Failure - confluence page update request error - non 200",
+			about{
+				Service: service{Name: "uw-service-refdata", Namespace: "billing"},
+				Doc: doc{
+					Name:        "uw-service-refdata",
+					Description: "uw-service-refdata",
+					Owners:      []owner{owner{Name: "Billing", Slack: "#billing"}},
+					Links:       []link{link{URL: "http://readme", Description: "readme"}},
+					BuildInfo:   buildInfo{Revision: "revision"},
+				}},
+			mockedClient{assert, map[string]httpCall{
+				"GET": httpCall{req: confluencePageRequest{confluenceURL + "/wiki/rest/api/content/" + confluencePageID, 1}, resp: http.Response{StatusCode: 200, Body: ioutil.NopCloser(strings.NewReader(confluencePageResponse(1)))}, err: nil},
+				"PUT": httpCall{req: confluencePageRequest{confluenceURL + "/wiki/rest/api/content/" + confluencePageID, 2}, resp: http.Response{StatusCode: 500, Body: ioutil.NopCloser(strings.NewReader("internal server error"))}, err: nil},
+			}}, fmt.Errorf("Could not update confluence page with ID 1234: (Confluence api returned status 500)"),
+		},
+	}
+
+	for _, test := range tests {
+		confluenceExporter, _ := newConfluenceExporter(confluenceURL, confluenceCredentials, confluencePageID, &test.client)
+		err := confluenceExporter.handle(test.ab)
+		assert.Equal(test.err, err)
+	}
+
+}
+
+func confluencePageResponse(pageVersion int) string {
+	return fmt.Sprintf(confluenceGetPageResponse, pageVersion)
+}
+
+type mockedClient struct {
+	assert *assert.Assertions
+	m      map[string]httpCall
+}
+
+type httpCall struct {
+	req  confluencePageRequest
+	resp http.Response
+	err  error
+}
+
+type confluencePageRequest struct {
+	url         string
+	pageVersion int
+}
+
+func (d *mockedClient) Do(req *http.Request) (resp *http.Response, err error) {
+	call := d.m[req.Method]
+	if call.err != nil {
+		return nil, call.err
+	}
+	d.assert.Equal(call.req.url, req.URL.String())
+	d.assert.Equal("Basic "+confluenceCredentials, req.Header["Authorization"][0])
+	if req.Method == "PUT" {
+		body, _ := ioutil.ReadAll(req.Body)
+		d.assert.Contains(string(body), fmt.Sprintf("\"version\":{\"number\":%d", call.req.pageVersion))
+	}
+	return &call.resp, nil
 }
